a4fd97b21720f392e1ae12ff277dd3c0c8caf357
ibigbug <xiaobayuwei@gmail.com>
2012-04-08 00:28:13 +0800

    modify ui

--- static/js/func.js
+++ static/js/func.js
@@ -1,4 +1,3 @@
-window.onerror = function(){ return true; }
 //rewrite setTimeout
 
 var _st = window.setTimeout;
@@ -26,8 +25,8 @@ $(document).ready(function(){
     //fetch feed
     $('.feed').click(function(){
         var id = $(this).attr('id').split('-')[1];
-        waterFlow._current = id;
-        waterFlow.init(id);
+        //waterFlow._current = id;
+        waterFlow.append(id);
     });
 
     // footer 
@@ -61,69 +60,17 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
-function random(limit) {
-    var limit = limit || 30;
-    return parseInt(Math.random()*limit+1);
-}
-function subString(str,len) {
-    var strlen = 0; 
-    var s = "";
-    for(var i = 0;i < str.length;i++){
-        if(str.charCodeAt(i) > 128){
-        strlen += 2;
-        }
-        else { 
-        strlen++;
-        }
-        s += str.charAt(i);
-        if(strlen >= len){ 
-            return s ;
-        }
-    }
-    return s;
-}
-
-function resize(){
-    $('.container').fadeOut(300).html('').fadeIn(400);
-    waterFlow._loadFinish = false;
-    waterFlow.detect().create().initAppend(waterFlow._current).onscroll();
-}
-
 var waterFlow = {
     debug: function(e){
         console.log(e);
     },
-    _isFirst: true,
-    _width: 300,
-    _current: null,
-    _loadFinish: false,
-    _index: 0,
-    reset: function(){
-        $('.container').data('data','');
-        this._isFirst = true;
-        this._loadFinish = false;
-        this._index = 0;
-        return this;
-    },
-    detect: function(){
-        this._index= 0,
-        this._loadFinish = false;
-        var containerWidth = $(window).width()-$('.container').offset().left;
-        this.columnNum = parseInt(containerWidth/this._width);
-        return this;
-    },
-    create: function(){
-        $('.container').html('');
-        var columnNum = this.columnNum;
-        columnHtml = '';
-        for(var i=0;i<columnNum;i++){
-            columnHtml += '<div class="column" id="column-'+i+'"></div>';
-        }
-        $('.container').append(columnHtml);
-        return this;
-    },
-    initAppend: function(id){
-        if(this._isFirst){
+    //_isFirst: true,
+    //_width: 300,
+    //_current: null,
+    //_loadFinish: false,
+    //_index: 0,
+    append: function(id){
+        //if(this._isFirst){
             req = $.ajax({
                 type: 'POST',
                 url: '/',
@@ -132,28 +79,26 @@ var waterFlow = {
                         _xsrf: getCookie('_xsrf'),
                     },
                 success: function(data){
-                    $('.container').data('data',data);
-                    waterFlow._index = 0;
-                    for (var j=0;j<5;j++){
-                        for(var i=0; i<waterFlow.columnNum;i++){
-                            if(waterFlow._loadFinish) return;
-                            var index = waterFlow._index;
-                            var html = '';
-                            html += '<article id="index-'+index+'">';
-                            html += '<h3><a href="/view/'+data.data[index]['id']+'">'+data.data[index]['title']+'</a></h3>';
-                            html += '<div class="meta"><time datetime="'+data.data[index].time+'">'+data.data[index].time+'</time> By '+data.data[index].author+'</div>'
-                            html += '<div class=content>'+subString(data.data[index]['content'],random(300))+'</content>';
-                            html += '</aticle>';
-                            html += '</div>';
-                            $('#column-'+i).append(html);
-                            waterFlow._isFirst = false;
-                            waterFlow._index++ ;
-                            if(waterFlow._index>=data.count) waterFlow._loadFinish = true;
-                        }
+                    //$('.container').data('data',data);
+                    //waterFlow._index = 0;
+                    var html = '';
+                    for (var j=0;j<data.count;j++){
+                        //if(waterFlow._loadFinish) return;
+                        //var index = waterFlow._index;
+                        html += '<article id="index-'+j+'">';
+                        html += '<h3><a href="'+data.data[j]['link']+'">'+data.data[j]['title']+'</a></h3>';
+                        html += '<div class="meta"><time datetime="'+data.data[j].time+'">'+data.data[j].time+'</time> By '+data.data[j].author+'</div>'
+                        html += '<div class=content>'+data.data[j]['content']+'</div>';
+                        html += '</article>';
+                        //waterFlow._isFirst = false;
+                        //waterFlow._index++ ;
+                        //if(waterFlow._index>=data.count) waterFlow._loadFinish = true;
                     }
+                    $('.container').append(html);
                 },
             });
-        }
+        //}
+        /*
         else{
             for (var j=0;j<5;j++){
                 for(var i=0; i<waterFlow.columnNum;i++){
@@ -171,23 +116,10 @@ var waterFlow = {
                 }
                 if(waterFlow._index>=data.count) waterFlow._loadFinish = true;
             }
-        }
+        }*/
         return this;
     },
-    buildHtml: function(){
-        if(!this._loadFinish&&!this._isFirst){
-            var data = $('.container').data('data');
-            var html = '';
-            html += '<article id="index-'+this._index+'">';
-            html += '<h3><a href="/view/'+data.data[this._index]['id']+'">'+data.data[this._index]['title']+'</a></h3>';
-            html += '<div class="meta"><time datetime="'+data.data[this._index].time+'">'+data.data[this._index].time+'</time> By '+data.data[this._index].author+'</div>'
-            html += '<div class=content>'+subString(data.data[this._index]['content'],random(300))+'</content>';
-            html += '</aticle>';
-            html += '</div>';
-            return html;
-        }
-        else return false;
-    },
+    /*
     onscroll: function(){
         window.onscroll = function(){
             var st = $('body').scrollTop();
@@ -209,8 +141,5 @@ var waterFlow = {
             }
         }
         return this;
-    },
-    init: function(id){
-        this.reset().detect().create().initAppend(id).onscroll();
-    },
+    },*/
 }
